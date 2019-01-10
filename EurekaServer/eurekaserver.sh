4afb4be52142163b2da48e3e68bd56d52cc57d4d
humingfeng <humf@vip.qq.com>
2019-01-10 15:35:54 +0800

    jenkins+docker

---
+++ EurekaServer/eurekaserver.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+echo "当前位置：" `pwd`
+echo "当前用户：" `whoami`
+docker -v
+#构建docker 镜像
+version=$(date +%Y%m%d)
+docker build -t eurekaserver:$version -f /home/jenkins/EurekaServer/Dockerfile .
+#推送docker镜像
+#docker push $DOCKER_REGISTRY$IMAGE_NAME
+#删除同名docker容器
+cid=$(docker ps -a| grep "eurekaserver" | awk '{print $1}')
+if [ "$cid" != "" ]; then
+docker rm -f $cid
+fi
+#启动docker 容器
+docker run -d -p 8761:8080 --name eurekaserver eurekaserver:$version
+#删除 Dockerfile 文件
+#rm -f Dockerfile
+#删除之前的同名镜像
+imagesid==$(docker images -f before=eurekaserver:$version -q)
+if [ "imagesid" != "" ]; then
+docker rmi -f $imagesid
+fi
---
+++ Feign/feign.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+echo "当前位置：" `pwd`
+echo "当前用户：" `whoami`
+docker -v
+#构建docker 镜像
+version=$(date +%Y%m%d)
+docker build -t feign:$version -f /home/jenkins/Feign/Dockerfile .
+#推送docker镜像
+#docker push $DOCKER_REGISTRY$IMAGE_NAME
+#删除同名docker容器
+cid=$(docker ps -a| grep "feign" | awk '{print $1}')
+if [ "$cid" != "" ]; then
+docker rm -f $cid
+fi
+#启动docker 容器
+docker run -d -p 8765:8080 --name feign feign:$version
+#删除 Dockerfile 文件
+#rm -f Dockerfile
+#删除之前的同名镜像
+imagesid==$(docker images -f before=feign:$version -q)
+if [ "imagesid" != "" ]; then
+docker rmi -f $imagesid
+fi

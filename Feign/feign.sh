2caa862850ba8f04515a8152a3bc77cffdb0b93e
HuMingfeng <humf@vip.qq.com>
2019-01-10 23:39:37 +0800

    Dockerfile

--- Feign/feign.sh
+++ Feign/feign.sh
@@ -4,6 +4,7 @@ echo "当前用户：" `whoami`
 docker -v
 #构建docker 镜像
 version=$(date +%Y%m%d)
+imagesid==$(docker images eurekaserver -q)
 docker build -t feign:$version -f /home/jenkins/Feign/Dockerfile .
 #推送docker镜像
 #docker push $DOCKER_REGISTRY$IMAGE_NAME
@@ -17,7 +18,6 @@ docker run -d -p 8765:8080 --name feign feign:$version
 #删除 Dockerfile 文件
 #rm -f Dockerfile
 #删除之前的同名镜像
-imagesid==$(docker images feign:$version -q)
 if [ "$imagesid" != "" ]; then
 docker rmi -f $imagesid
 fi

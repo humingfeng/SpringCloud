84fe01bfc71be4d7ae3449477ceafebb16baa5d4
HuMingfeng <humf@vip.qq.com>
2019-01-14 23:57:01 +0800

    RestRibbon添加断路器hystrix

--- RestRibbon/src/main/java/com/humingfeng/restribbon/service/RestService.java
+++ RestRibbon/src/main/java/com/humingfeng/restribbon/service/RestService.java
@@ -1,5 +1,6 @@
 package com.humingfeng.restribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,8 +18,14 @@ public class RestService {
     @Autowired
     private RestTemplate restTemplate;
 
+
+    @HystrixCommand(fallbackMethod = "homeError")
     public String getHomeService(String name) {
         return restTemplate.getForObject("http://eureka-client-first/home?name="+name,String.class);
     }
 
+
+    public String homeError(String name) {
+        return "hi,"+name+",sorry,error!notice:hystrix";
+    }
 }

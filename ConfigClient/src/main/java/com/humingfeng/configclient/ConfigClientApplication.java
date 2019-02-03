96c2020740a7ef4ea9d4fa3e64be1dc837e05016
humingfeng <humf@vip.qq.com>
2019-02-03 21:01:56 +0800

    高可用的分布式配置中心

--- ConfigClient/src/main/java/com/humingfeng/configclient/ConfigClientApplication.java
+++ ConfigClient/src/main/java/com/humingfeng/configclient/ConfigClientApplication.java
@@ -3,11 +3,13 @@ package com.humingfeng.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
